fa6f90595bc576fffe9ca8443771ba58ffc39cbc
Nurdoidz <nurdoidz@live.com>
2023-09-05 18:53:55 -0600

    Add Spring Boot to application

---
+++ src/main/java/com/brosintime/tvtracker/TvTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.brosintime.tvtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brosintime/tvtracker/TvtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brosintime.tvtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
